b6890cbe71a65a9ff7906c870044c6861d1d50c1
jokamjohn <johnkagga@gmail.com>
2015-03-16 12:56:42 +0300

    updated contract

--- app/src/main/java/com/example/android/sunshine/app/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/app/WeatherContract.java
@@ -1,11 +1,32 @@
 package com.example.android.sunshine.app;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
  * Created by DELL on 3/11/2015.
  */
+
 public class WeatherContract {
+    // The "Content authority" is a name for the entire content provider, similar to the
+    // relationship between a domain name and its website.  A convenient string to use for the
+    // content authority is the package name for the app, which is guaranteed to be unique on the
+    // device.
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.sunshine.app";
+    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+    // the content provider.
+
+    public static final Uri BASE_CONTENT_URI =Uri.parse("content:// " + CONTENT_AUTHORITY);
+    // Possible paths (appended to base content URI for possible URI's)
+    // For instance, content://com.example.android.sunshine.app/weather/ is a valid path for
+    // looking at weather data. content://com.example.android.sunshine.app/givemeroot/ will fail,
+    // as the ContentProvider hasn't been given any information on what to do with "givemeroot".
+    // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
+
+    public static final String PATH_WEATHER = "weather";
+    public static final String PATH_LOCATION ="location";
+
     public static final class WeatherEntry implements BaseColumns {
         public static final String TABLE_NAME = "weather";
         public static final String COLUMN_LOC_KEY ="location_id";
