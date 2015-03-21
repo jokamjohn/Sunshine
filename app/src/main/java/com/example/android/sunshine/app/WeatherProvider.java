fb5c84a33589013cb6e604250622b560efca99e5
jokamjohn <johnkagga@gmail.com>
2015-03-21 10:40:22 +0300

    writing the UriMatcher

--- app/src/main/java/com/example/android/sunshine/app/WeatherProvider.java
+++ app/src/main/java/com/example/android/sunshine/app/WeatherProvider.java
@@ -2,6 +2,7 @@ package com.example.android.sunshine.app;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -17,6 +18,28 @@ public class WeatherProvider extends ContentProvider {
     private static final int LOCATION = 300;
     private static final int LOCATION_ID = 301;
 
+    //adding our class variable
+    //URI matcher used by the content provider
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    private static UriMatcher buildUriMatcher(){
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        //shortcut to the weather contract content authority
+        final String authority = WeatherContract.CONTENT_AUTHORITY;
+
+        //adding the uri, with the corresponding code
+        matcher.addURI(authority,WeatherContract.PATH_WEATHER,WEATHER);
+        matcher.addURI(authority,WeatherContract.PATH_WEATHER +"/*",WEATHER_WITH_LOCATION);
+        matcher.addURI(authority,WeatherContract.PATH_WEATHER +"/*/*",WEATHER_WITH_LOCATION_AND_DATE);
+        matcher.addURI(authority,WeatherContract.PATH_LOCATION,LOCATION);
+        matcher.addURI(authority,WeatherContract.PATH_LOCATION +"/#",LOCATION_ID);
+        // /* is for string because our initial data type was a string
+        // /# an integer and for this case the ID is always a along integer
+        return matcher;
+
+
+
+    }
 
     @Override
     public boolean onCreate() {
