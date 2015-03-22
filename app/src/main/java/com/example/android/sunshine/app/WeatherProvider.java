66e4616ce8d0e82e179468cfafc404105d72eb66
jokamjohn <johnkagga@gmail.com>
2015-03-22 12:36:04 +0300

    Coding the content provider efficient updatesInserts

--- app/src/main/java/com/example/android/sunshine/app/WeatherProvider.java
+++ app/src/main/java/com/example/android/sunshine/app/WeatherProvider.java
@@ -286,7 +286,7 @@ public class WeatherProvider extends ContentProvider {
                 rowsUpdated=db.update(WeatherContract.WeatherEntry.TABLE_NAME,values,selection,selectionArgs);
                 break;
             case LOCATION:
-                rowsUpdated=db.update(WeatherContract.LocationEntry.TABLE_NAME,values,selection,selectionArgs);
+                rowsUpdated=db.update(WeatherContract.LocationEntry.TABLE_NAME, values, selection, selectionArgs);
                 break;
             default:
                 throw new UnsupportedOperationException("Unknown uri: "+uri);
@@ -299,5 +299,35 @@ public class WeatherProvider extends ContentProvider {
         }
 
         return rowsUpdated;
+    }
+
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        final SQLiteDatabase db =mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        switch (match){
+            case WEATHER:
+                db.beginTransaction();
+                int returnCount =0;
+                try {
+                    for (ContentValues value : values){
+                        long _id = db.insert(WeatherContract.WeatherEntry.TABLE_NAME,null,value);
+                        if (-1 !=_id){
+                            returnCount++;
+                        }
+
+                    }
+                    db.setTransactionSuccessful();
+                }
+                finally {
+                    db.endTransaction();
+                }
+                getContext().getContentResolver().notifyChange(uri,null);
+
+                default:
+                    return super.bulkInsert(uri, values);
+
+        }
+
     }
 }
