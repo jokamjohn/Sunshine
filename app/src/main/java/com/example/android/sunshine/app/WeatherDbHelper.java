58ad73c2506ab6023043cc45a54bde7f81b75a55
jokamjohn <johnkagga@gmail.com>
2015-04-04 15:44:58 +0300

    Fix WeatherDbHelper

--- app/src/main/java/com/example/android/sunshine/app/WeatherDbHelper.java
+++ app/src/main/java/com/example/android/sunshine/app/WeatherDbHelper.java
@@ -51,6 +51,8 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
                 "UNIQUE("+ WeatherContract.WeatherEntry.COLUMN_DATETEXT+","+ WeatherContract.WeatherEntry.COLUMN_LOC_KEY+")ON CONFLICT REPLACE);";
 
         db.execSQL(SQL_CREATE_WEATHER_TABLE);
+        db.execSQL(SQL_CREATE_LOCATION_TABLE);
+
 
 
     }
